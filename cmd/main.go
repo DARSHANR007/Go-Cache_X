c10cd278d1e222a9f68d89d21ee86f4bbe9ee54e
darsh <darshanravichandran07@gmail.com>
2025-10-30 21:54:51 +0530

    Refactor main and add cache test in tests package
    Moved the cache testing logic from main.go to a new Test1 function in the tests package. main.go now calls tests.Test1, improving code organization and separation of concerns.

--- cmd/main.go
+++ cmd/main.go
@@ -2,38 +2,10 @@ package main
 
 import (
 	"fmt"
-	"go_cache/internals/cache"
-	"time"
+	"go_cache/tests"
 )
 
 func main() {
-	c := cache.NewCache(3 * time.Second)
-
-	c.Set("user1", "Darsh", 5*time.Second)
-	c.Set("user2", "Alice", 2*time.Second)
-	c.Set("user3", "Bob", 7*time.Second)
-	c.Set("user4", "Charlie", 10*time.Second)
-	c.Set("user5", "Eve", 1*time.Second)
-	c.Set("user6", "Mallory", 8*time.Second)
-	c.Set("user7", "Trudy", 3*time.Second)
-	c.Set("user8", "Victor", 4*time.Second)
-	c.Set("user9", "Peggy", 12*time.Second)
-	c.Set("user10", "Sybil", 6*time.Second)
-
-	fmt.Println("✅ Added 10 users to cache")
-
-	time.Sleep(15 * time.Second)
-
-	fmt.Println("\n🔍 Checking who is still alive in cache:")
-
-	for i := 1; i <= 10; i++ {
-		key := fmt.Sprintf("user%d", i)
-		if val, ok := c.GetItem(key); ok {
-			fmt.Printf("✅ %s found: %v\n", key, val)
-		} else {
-			fmt.Printf(" %s expired or missing\n", key)
-		}
-	}
-
-	c.FinishCleanup()
+	fmt.Println("🚀 Running cache test...")
+	tests.Test1() // calling the test from tests package
 }
---
+++ tests/test1.go
@@ -0,0 +1,39 @@
+package tests
+
+import (
+	"fmt"
+	"go_cache/internals/cache"
+	"time"
+)
+
+func Test1() {
+	c := cache.NewCache(3 * time.Second)
+
+	c.Set("user1", "Darsh", 5*time.Second)
+	c.Set("user2", "Alice", 2*time.Second)
+	c.Set("user3", "Bob", 7*time.Second)
+	c.Set("user4", "Charlie", 10*time.Second)
+	c.Set("user5", "Eve", 1*time.Second)
+	c.Set("user6", "Mallory", 8*time.Second)
+	c.Set("user7", "Trudy", 3*time.Second)
+	c.Set("user8", "Victor", 4*time.Second)
+	c.Set("user9", "Peggy", 12*time.Second)
+	c.Set("user10", "Sybil", 6*time.Second)
+
+	fmt.Println("✅ Added 10 users to cache")
+
+	time.Sleep(15 * time.Second)
+
+	fmt.Println("\n🔍 Checking who is still alive in cache:")
+
+	for i := 1; i <= 10; i++ {
+		key := fmt.Sprintf("user%d", i)
+		if val, ok := c.GetItem(key); ok {
+			fmt.Printf("✅ %s found: %v\n", key, val)
+		} else {
+			fmt.Printf(" %s expired or missing\n", key)
+		}
+	}
+
+	c.FinishCleanup()
+}
