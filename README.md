660fcb3b45680adc91c59c1d307af7e5d3627134
darsh <darshanravichandran07@gmail.com>
2025-10-30 13:40:34 +0530

    Initial commit of Go Cache project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Go-Cache_X
+⚡ A lightweight in-memory caching library written in Go. Supports TTL expiration, thread-safe access, and easy extensibility for future features like LRU and clustering.
---
+++ internals/cache/cache.go
@@ -0,0 +1,74 @@
+package cache
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Item struct {
+	CacheValue interface{}
+	Expiration int64
+}
+
+type Cache struct {
+	data map[string]Item
+	mu   sync.RWMutex
+}
+
+// Constructor of cache
+func NewCache() *Cache {
+
+	return &Cache{
+		data: make(map[string]Item),
+	}
+
+}
+
+func (c *Cache) Set(value interface{}, key string, ttl time.Duration) {
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	var expTime int64
+
+	if ttl > 0 {
+		expTime = time.Now().Add(ttl).Unix()
+	}
+
+	c.data[key] = Item{CacheValue: value, Expiration: expTime}
+
+}
+
+func (c *Cache) getItem(key string) (*interface{}, bool) {
+
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	item, found := c.data[key]
+
+	if !found {
+		fmt.Println("Error in retrieving")
+		fmt.Errorf("Item not found")
+		return nil, false
+
+	}
+
+	if item.Expiration > 0 && time.Now().Unix() > item.Expiration {
+
+		fmt.Errorf(" Item has been expired")
+
+		return nil, false
+	}
+
+	return &item.CacheValue, true
+
+}
+
+func (c *Cache) DeleteCache(key string) {
+
+	c.mu.Lock()
+	defer c.mu.Lock()
+	delete(c.data, key)
+
+}
