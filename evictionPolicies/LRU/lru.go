a931487d72f377265aee389e3533e4286d75637a
darsh <darshanravichandran07@gmail.com>
2025-10-31 13:01:48 +0530

    Add LRU cache implementation with basic operations
    Introduces a Cache struct with Get, Set, and Display methods for an LRU eviction policy. The cache uses a doubly linked list and a map to manage items and maintain order, supporting capacity-based eviction and value updates.

--- evictionPolicies/LRU/lru.go
+++ evictionPolicies/LRU/lru.go
@@ -1,1 +1,59 @@
 package lru
+
+import "fmt"
+
+type Cache struct {
+	capacity int
+	items    map[string]*Node
+	list     *DoublyLinkedList
+}
+
+func New(capacity int) *Cache {
+	return &Cache{
+		capacity: capacity,
+		items:    make(map[string]*Node),
+		list:     &DoublyLinkedList{},
+	}
+}
+
+func (c *Cache) Get(key string) (interface{}, bool) {
+	if node, found := c.items[key]; found {
+		c.list.MoveToFront(node)
+		fmt.Println("Successfull cache hit")
+		return node.value, true
+	}
+
+	return nil, false
+}
+
+func (c *Cache) Set(key string, value interface{}) bool {
+
+	if node, found := c.items[key]; found { //update, set new
+		node.value = value
+		c.list.MoveToFront(node)
+		fmt.Println("key already present,updated")
+		return true
+	}
+
+	if len(c.items) >= c.capacity {
+
+		removed := c.list.RemoveFromTail()
+		delete(c.items, removed.key)
+
+	}
+
+	newNode := &Node{key: key, value: value}
+	c.list.AddtoFront(newNode)
+	c.items[key] = newNode
+	fmt.Println("added new node")
+	return true
+
+}
+
+func (c *Cache) Display() {
+	fmt.Print("Cache state (MRU → LRU): ")
+	for node := c.list.head; node != nil; node = node.next {
+		fmt.Printf("[%s:%v] ", node.key, node.value)
+	}
+	fmt.Println()
+}
